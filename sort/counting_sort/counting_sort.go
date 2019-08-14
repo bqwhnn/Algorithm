e9de51f82411e89f422b8a6338780098d66deb92
caiyangmin <18819457397@163.com>
2019-08-14 18:46:52 +0800

    修改计数排序

--- sort/counting_sort/counting_sort.go
+++ sort/counting_sort/counting_sort.go
@@ -3,10 +3,10 @@ package algorithm
 func countingSort(nums []int) {
 	len := len(nums)
 	min, max := nums[0], nums[0]
-	m := make(map[int]int)
+	bucket := make(map[int]int)
 
 	for i := 0; i < len; i++ {
-		m[nums[i]]++
+		bucket[nums[i]]++
 		if nums[i] < min {
 			min = nums[i]
 		}
@@ -17,7 +17,7 @@ func countingSort(nums []int) {
 
 	k := 0
 	for i := min; i <= max; i++ {
-		for count := m[i]; count > 0; count-- {
+		for count := bucket[i]; count > 0; count-- {
 			nums[k] = i
 			k++
 		}
