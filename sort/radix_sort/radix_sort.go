9c826d2597a7e075b2a03496f181bd59442babc4
caiyangmin <18819457397@163.com>
2019-08-16 18:19:27 +0800

    基数排序

--- sort/radix_sort/radix_sort.go
+++ sort/radix_sort/radix_sort.go
@@ -1,6 +1,32 @@
 package algorithm
 
-// RadixSort is radix sort
-func RadixSort(nums []int) {
+// RadixSort is LSD radix sort
+func RadixSort(a []int) []int {
+	n, m := len(a), a[0]
+	divisor := 1
 
+	for i := 0; i < n; i++ {
+		if a[i] > m {
+			m = a[i]
+		}
+	}
+
+	for m/divisor > 0 {
+		bucket := make([][]int, 10)
+		for i := 0; i < n; i++ {
+			k := a[i] / divisor % 10
+			b := bucket[k]
+			b = append(b, a[i])
+			bucket[k] = b
+		}
+		for i, j := 0, 0; i < 10; i++ {
+			b := bucket[i]
+			for _, v := range b {
+				a[j] = v
+				j++
+			}
+		}
+		divisor *= 10
+	}
+	return a
 }
--- sort/radix_sort/radix_sort_test.go
+++ sort/radix_sort/radix_sort_test.go
@@ -2,13 +2,15 @@ package algorithm
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
 
 func TestRadixSort(t *testing.T) {
-	nums := []int{321, 1, 10, 60, 743, 127, 577}
+	nums := []int{321, 1, 10, 60, 743, 127, 577, 2, 24, 45, 66, 75, 90, 170, 802}
 
 	fmt.Println("before radix sort", nums)
-	RadixSort(nums)
+	nums = RadixSort(nums)
 	fmt.Println("after radix sort", nums)
+	fmt.Println("issorted", sort.IntsAreSorted(nums))
 }
