a3bd150147c8488ec9e9a17f02c4a14fcaf13ef2
caiyangmin <18819457397@163.com>
2019-08-16 16:53:36 +0800

    桶排序

--- sort/bucket_sort/bucket_sort.go
+++ sort/bucket_sort/bucket_sort.go
@@ -1,8 +1,42 @@
 package algorithm
 
-import ()
+import (
+	"math"
+)
+
+const (
+	bc = 10 // 桶的数量
+)
 
 // BucketSort is bucket sort
-func BucketSort(nums []int) {
+func BucketSort(a []int) []int {
+	k, n := a[0], len(a)
+	bucket := make([][]int, bc)
+	c := make([]int, n)
 
+	for i := 0; i < n; i++ {
+		if a[i] > k {
+			k = a[i]
+		}
+	}
+
+	m := int(math.Ceil(float64(k)/bc))
+	for i := 0; i < n; i++ {
+		bn := a[i] / m // 计算放在哪个桶
+		b := bucket[bn]
+		b = append(b, a[i])
+		for j := len(b)-1; j > 0 && b[j] < b[j-1]; j-- {
+			b[j], b[j-1] = b[j-1], b[j]
+		}
+		bucket[bn] = b
+	}
+
+	for i, j := 0, 0; i < bc; i++ {
+		b := bucket[i]
+		blen := len(b)
+		copy(c[j:j+blen], b[0:blen])
+		j += blen
+	}
+
+	return c
 }
--- sort/bucket_sort/bucket_sort_test.go
+++ sort/bucket_sort/bucket_sort_test.go
@@ -3,12 +3,14 @@ package algorithm
 import (
 	"fmt"
 	"testing"
+	"sort"
 )
 
 func TestBucketSort(t *testing.T) {
-	nums := []int{29, 25, 3, 49, 9, 37, 21, 43, 69, 58, 1}
+	nums := []int{29, 25, 3, 49, 9, 10, 5, 4, 9, 37, 21, 43, 69, 58, 1}
 
 	fmt.Println("before bucket sort", nums)
-	BucketSort(nums)
+	nums = BucketSort(nums)
 	fmt.Println("after bucket sort", nums)
+	fmt.Println("issorted", sort.IntsAreSorted(nums))
 }
