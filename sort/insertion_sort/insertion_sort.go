18955d8372f2ba52a15af095625f69f5020b7451
caiyangmin <18819457397@163.com>
2019-11-08 22:46:19 +0800

    优化插入排序

--- sort/insertion_sort/insertion_sort.go
+++ sort/insertion_sort/insertion_sort.go
@@ -1,11 +1,28 @@
 package algorithm
 
 // InsertionSort is insertion sort
-func InsertionSort(nums []int) {
-	len := len(nums)
+func InsertionSort(a []int) {
+	len := len(a)
 	for i := 1; i < len; i++ {
-		for j := i - 1; j >= 0 && nums[j] > nums[j+1]; j-- {
-			nums[j], nums[j+1] = nums[j+1], nums[j]
+		for j := i - 1; j >= 0 && a[j] > a[j+1]; j-- {
+			a[j], a[j+1] = a[j+1], a[j]
 		}
+	}
+}
+
+// InsertionSort2 减少了交换操作
+func InsertionSort2(a []int) {
+	len := len(a)
+	for i := 1; i < len; i++ {
+		j := i - 1
+		v := a[i]
+		for ; j >= 0; j-- {
+			if a[j] > v {
+				a[j+1] = a[j]
+			} else {
+				break
+			}
+		}
+		a[j+1] = v
 	}
 }
